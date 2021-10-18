1169f54281dcb4ba482c43b58e4b7dbf88338adf
i.zemlyachenko <i.zemlyachenko@tinkoff.ru>
2021-10-18 15:28:24 +0300

    Add more tests

--- test/basic.test.js
+++ test/basic.test.js
@@ -6,24 +6,42 @@ const React = require('react') // used in eval
 
 const ex1 = `<div><div></div></div>`
 
-const a = babel.transformSync(ex1, {
+const ex2 = `let App = (p) => <div>{props.children}</div>; <App><div></div></App>`
+
+const ex1b = babel.transformSync(ex1, {
+  presets: ['@babel/preset-react'],
+  plugins: [plugin]
+})
+
+const ex2b = babel.transformSync(ex2, {
   presets: ['@babel/preset-react'],
   plugins: [plugin]
 })
 
 describe('Basic functionality', () => {
-  describe('node name', function() {
+  describe('DOM node name', function() {
     it('should be read', function() {
-      const output = eval(a.code)
+      const output = eval(ex1b.code)
       assert.equal(output.props['data-id'], '__div')
-      assert.equal(output.props.children.props['data-id'], '__div_1')
+    })
+  })
+
+  describe('React node name', function() {
+    it('should be read', function() {
+      const output = eval(ex2b.code)
+      assert.equal(output.props['data-id'], '__App')
     })
   })
 
   describe('index', function() {
     it('should be count and put into the tree', function() {
-      const output = eval(a.code)
+      const output = eval(ex1b.code)
       assert.equal(output.props.children.props['data-id'], '__div_1')
+    })
+
+    it('should not be count in case of different nodes', function() {
+      const output = eval(ex2b.code)
+      assert.equal(output.props.children.props['data-id'], '__div')
     })
   })
 })
